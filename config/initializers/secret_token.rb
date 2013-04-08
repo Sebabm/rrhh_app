97616de9302616bf394126cc94f98eccbae7f445
Sebabm <sebastian.vitangeli@gmail.com>
2013-04-08 19:19:37 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RrhhApp::Application.config.secret_token = 'efc46693d76a643b403784d20e1b4214510b0bc598ae9ae789652386afed2e411d69e65a216902acd7acfe637531471b6a3ee44682381621af9ff47bb2cd5524'
